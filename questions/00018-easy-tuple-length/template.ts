c42ebedf963495883119328be42489c89a559ae9
pietrushka <piotrekwisnia18@gmail.com>
2023-11-23 20:49:37 +0100

    Solve #18 easy

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,7 @@
-type Length<T> = any
+type Length<T extends readonly unknown[]> = T['length']
+
+// another solution from video
+// type Length<T extends readonly unknown[]> =
+//     T extends { length: infer L }
+//       ? L
+//       : never
