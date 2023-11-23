9fffd3424ce3afcc25255ba2539a0b09629120cf
pietrushka <piotrekwisnia18@gmail.com>
2023-11-23 19:38:29 +0100

    Solve #3

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,3 @@
-type MyOmit<T, K> = any
+type MyExlude1<T, K> = T extends K ? never : T
+type MyPick1<T, K extends keyof T> = { [P in K]: T[P] }
+type MyOmit<T, K extends keyof T> = MyPick1<T, MyExlude1<keyof T, K>>
