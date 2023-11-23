d25a0d07516284ec33c8737a91d4bdbdbf39d6c3
pietrushka <piotrekwisnia18@gmail.com>
2023-11-23 21:47:42 +0100

    Solve 189 easy

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,7 @@
-type MyAwaited<T> = any
+type Thenable<T> = { then: (onfulfilled: (...args: T[]) => unknown) => unknown }
+type ExtendedPromise<T> = Promise<T> | Thenable<T>
+type MyAwaited<T extends ExtendedPromise<unknown>> = T extends ExtendedPromise<infer U>
+  ? U extends ExtendedPromise<unknown>
+    ? MyAwaited<U>
+    : U
+  : T
