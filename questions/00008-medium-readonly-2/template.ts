705cf8d396d8aa93a9e9d36e54df9d5d15c0d698
pietrushka <piotrekwisnia18@gmail.com>
2023-11-25 19:33:44 +0100

    Solve #8 medium

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,22 @@
-type MyReadonly2<T, K> = any
+// type MyReadonly2<T, K extends keyof T = keyof T > =
+//     Readonly<Pick<T, K>> & Omit<T, K>
+
+type Readonly8<T> = {
+  readonly [key in keyof T]: T[key]
+}
+type Exlude8<T, K> = T extends K ? never : T
+type Pick8<T, K extends keyof T> = {
+  [P in K]: T[P];
+}
+type Omit8<T, K> = Pick8<T, Exlude8<keyof T, K>>
+
+type MyReadonly2<T, K extends keyof T = keyof T > =
+    Readonly<Pick<T, K>> & Omit<T, K>
+
+// my not perfect solution
+// type MyReadonly2<T, K extends keyof T = keyof T > = {
+//   readonly [readonlyKey in K]: T[readonlyKey];
+// } & {
+//   [key in Exlude8<keyof T, K>]: T[key];
+// }
+
