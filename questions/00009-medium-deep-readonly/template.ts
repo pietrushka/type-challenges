a24660580bf7ff54c3be6c31e0ab9395a80b9df7
pietrushka <piotrekwisnia18@gmail.com>
2023-11-26 19:17:18 +0100

    Solved #9 medium

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,11 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T> = T extends Record<PropertyKey, unknown>
+  ? {
+      readonly [P in keyof T]: T[P] extends Record<PropertyKey, unknown>
+        ? DeepReadonly<T[P]>
+        : T[P] extends Array<unknown>
+          ? DeepReadonly<T[P]>
+          : T[P]
+    }
+  : T extends [infer Head, ...infer Tail]
+    ? Readonly<[DeepReadonly<Head>, ...DeepReadonly<Tail>]>
+    : Readonly<T>
